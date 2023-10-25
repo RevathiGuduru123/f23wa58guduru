811fbdbac6ccbf70547b35318460da5317abe692
RevathiGuduru123 <RevathiGuduru123@users.noreply.github.com>
2023-10-25 18:23:15 -0500

    create computation

--- routes/computation.js
+++ routes/computation.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-module.exports = router;
+
 router.get('/', function(req, res, next) {
   const x = req.query.x ? parseFloat(req.query.x) : Math.random(); // Use query value or generate a random value
 
@@ -13,5 +13,7 @@ router.get('/', function(req, res, next) {
   const responseString = `${Math.cosh.name} applied to ${x} is ${result}`;
 
   res.send(responseString);
-  res.render('computation', { title: 'computation' });
 });
+
+
+module.exports = router;
