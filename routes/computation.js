03faca1e513396e7487c52d42f75428b1da8b66a
RevathiGuduru123 <RevathiGuduru123@users.noreply.github.com>
2023-10-25 17:43:14 -0500

    create computation

--- routes/computation.js
+++ routes/computation.js
@@ -1,25 +1,17 @@
-const express = require('express');
-const app = express();
-const port = 3000; // Change this to the desired port
+var express = require('express');
+var router = express.Router();
 
-app.use(express.json());
+/* GET home page. */
+module.exports = router;
+router.get('/', function(req, res, next) {
+  const x = req.query.x ? parseFloat(req.query.x) : Math.random(); // Use query value or generate a random value
 
-app.get('/computation', (req, res) => {
-  // Check if the 'x' query parameter is present in the request
-  const x = parseFloat(req.query.x);
+  // Calculate the result using Math.exp()
+  const result = Math.cosh(x);
 
-  if (!isNaN(x)) {
-    // If 'x' is provided in the query, use it
-    const result = Math.cosh(x);
-    res.send(`cosh() applied to ${x} is ${result}`);
-  } else {
-    // If 'x' is not provided, generate a random value between -10 and 10
-    const randomX = Math.random() * 20 - 10;
-    const result = Math.cosh(randomX);
-    res.send(`cosh() applied to a random value ${randomX} is ${result}`);
-  }
-});
+  // Prepare the response string
+  const responseString = `${Math.exp.name} applied to ${x} is ${result}`;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  res.send(responseString);
+  res.render('computation', { title: 'computation' });
 });
